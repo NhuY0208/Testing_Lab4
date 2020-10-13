b3cdcda36339ef468cc892013d35f46565cebdff
NhuY0208 <nguyenthinhuynguyen0208@gmail.com>
2020-10-13 21:21:43 +0700

    lan 1

---
+++ Test Suites/TS_Profile.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Profile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>98d49cd3-3d31-4813-8662-6ebcef50b97d</testSuiteGuid>
+   <testCaseLink>
+      <guid>63ec18b0-0f55-460e-9457-cae10a3abbb9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_Profile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
